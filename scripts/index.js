0b95cc9652d74cd588aae72edfd51ddbd5921137
Daria Pavlova <dmpavlova@yandex.ru>
2020-10-06 22:42:26 +0300

    last version

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,28 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.edit-button');
-let editForm = document.querySelector('.edit-form');
-let submitButton = editForm.querySelector('.edit-form__submit-button');
 let closeButton = popup.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__container');
-let profile = document.querySelector('.profile-info');
-let nameInput = editForm.querySelector('.edit-form__name');
-let savedNameInput = profile.querySelector('.profile-info__title');
-let jobInput = editForm.querySelector('.edit-form__about-yourself');
-let savedJobInput = profile.querySelector('.profile-info__subtitle');
+let formElement = document.querySelector('.form-container');
+let nameInput = document.querySelector('.edit-form__name');
+let savedNameInput = document.querySelector('.profile-info__title');
+let jobInput = document.querySelector('.edit-form__about-yourself');
+let savedJobInput = document.querySelector('.profile-info__subtitle');
 
-function OpenClosePopup(){
-popup.classList.toggle('popup__opened');
+function openPopup(){
+    nameInput.value = savedNameInput.textContent;
+    jobInput.value = savedJobInput.textContent;
+    popup.classList.add('popup_opened');
+}
+
+function closePopup(){
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    savedNameInput.textContent = nameInput.value
-    savedJobInput.textContent = jobInput.value
-    OpenClosePopup()
+    savedNameInput.textContent = nameInput.value;
+    savedJobInput.textContent = jobInput.value;
+    closePopup()
 }
-editButton.addEventListener('click', OpenClosePopup);
-closeButton.addEventListener('click', OpenClosePopup);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
